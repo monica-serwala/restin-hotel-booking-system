7986a6c22d27d45e92086108a07dc2ab99091f4d
monica serwala <serwalam@gmail.com>
2026-01-07 21:40:39 +0200

    Add ApplicationDbContext and EF Core configuration

---
+++ HotelBookingManagementSystem/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using HotelBookingManagementSystem.Models;
+
+namespace HotelBookingManagementSystem.Data
+
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Bedroom> Bedrooms { get; set; }
+        public DbSet<Booking> Bookings { get; set; }
+    }
+}
